124247a8f35277fe99541c40a7fb3745f1ed0c7d
Joaquin <joaquin.segura98@gmail.com>
2026-03-18 20:08:08 -0300

    Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -731,6 +731,18 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/images/logo-nexus.png" />
         <link rel="shortcut icon" href="/images/logo-nexus.png" />
         
+      {/* Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "vtuj7bivup");`,
+          }}
+        />
+
         {/* Schema.org - Organization */}
         <script
           type="application/ld+json"
